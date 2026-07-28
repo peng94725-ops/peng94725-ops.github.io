09802385fa0dad526ad23e38ab2c6e2e8af3e4ee
peng94725-ops <peng94725@gmail.com>
2026-07-28 16:37:53 +0800

    add sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "move-inventory-v7";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./db.js",
+  "./xlsx-maker.js",
+  "./export-excel.js",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.url.includes("/api/")) return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      return (
+        cached ||
+        fetch(e.request).then((resp) => {
+          if (resp.ok) {
+            const clone = resp.clone();
+            caches.open(CACHE_NAME).then((c) => c.put(e.request, clone));
+          }
+          return resp;
+        }).catch(() => cached)
+      );
+    })
+  );
+});
